7f00ede1c58d89ac9c927519621ad646d4458fce
Michael J. Armstrong <40207613+mjathecpa@users.noreply.github.com>
2023-03-25 22:38:28 -0400

    working csv>map>display

--- gocsvtomap.go
+++ gocsvtomap.go
@@ -6,9 +6,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
-func openCsvFile(fileName string) *csv.Reader {
+func OpenCsvFile(fileName string) *csv.Reader {
 	// Load a csv file.
 	f, _ := os.Open(fileName)
 
@@ -16,10 +17,10 @@ func openCsvFile(fileName string) *csv.Reader {
 	r := csv.NewReader(bufio.NewReader(f))
 	r.Comma = ','
 
-	return r*
+	return r
 }
 
-func spitout(csvData *csv.Reader) {
+func DataToConsole(csvData *csv.Reader) {
 	for {
 		record, err := csvData.Read()
 		// Stop at EOF.
@@ -32,13 +33,60 @@ func spitout(csvData *csv.Reader) {
 		fmt.Println(record)
 		fmt.Println(len(record))
 		for value := range record {
-			fmt.Printf("  %v\n", record[value])
+			fmt.Printf("  %v,", record[value])
 		}
 		//fmt.Println(record[2]) // display position 2 items
 	}
 }
 
+// CSVFileToMap  reads csv file into slice of map
+// slice is the line number
+// map[string]string where key is column name
+func CSVFileToMap(csvData *csv.Reader) (returnMap []map[string]string, err error) {
+
+	rawCSVdata, err := csvData.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf(err.Error())
+	}
+
+	header := []string{} // holds first row (header)
+	for lineNum, record := range rawCSVdata {
+
+		// for first row, build the header slice
+		if lineNum == 0 {
+			for i := 0; i < len(record); i++ {
+				header = append(header, strings.TrimSpace(record[i]))
+			}
+		} else {
+			// for each cell, map[string]string k=header v=value
+			line := map[string]string{}
+			for i := 0; i < len(record); i++ {
+				line[header[i]] = record[i]
+			}
+			returnMap = append(returnMap, line)
+		}
+	}
+
+	return
+}
+
+func PrintMap(dataMap []map[string]string) {
+	// loop over elements of slice
+	for _, m := range dataMap {
+
+		// m is a map[string]interface.
+		// loop over keys and values in the map.
+		for k, v := range m {
+			fmt.Println(k, "value is", v)
+		}
+	}
+}
+
 func main() {
-	f := openCsvFile("./testdata.csv")
-	spitout(f)
+	f := OpenCsvFile("./testdata_short.csv")
+	//DataToConsole(f)
+	m, _ := CSVFileToMap(f)
+
+	PrintMap(m)
+
 }
